cd2799f89b06f302532f40c35a61e7e25ccbf90e
trungquan17 <quanlightning17@gmail.com>
2018-04-12 16:37:12 +0700

    init project

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/handler.ts',
+    target: 'node',
+    stats: {
+        warnings: false
+    },
+    module: {
+        exprContextCritical: false,
+        loaders: [{
+            test: /\.ts(x?)$/,
+            loader: 'ts-loader',
+            exclude: [/node_modules/, /\.(spec|e2e)\.ts$/]
+        }, ]
+    },
+
+    resolve: {
+        extensions: ['.ts', '.js', '.tsx', '.jsx']
+    },
+
+    output: {
+        libraryTarget: 'commonjs',
+        path: path.join(__dirname, 'dist'),
+        filename: 'handler.js'
+    },
+
+    externals: {
+        'aws-sdk': true
+    },
+};
